67ab73fc3fdf523aa1a8ef982f8263966e8c2a34
ALASTAIRMIC3B23\alastairmicallef <mezigan@gmail.com>
2018-12-04 19:35:00 +0100

    Added the ConnectionClass

---
+++ DataAccess/ConnectionClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace DataAccess
+{
+    public class ConnectionClass
+    {
+        public MusicSharingDBEntities Entity { get; set;}
+
+        public ConnectionClass()
+        {
+            Entity = new MusicSharingDBEntities();
+        }
+        
+        public ConnectionClass(bool isAdmin)
+        {
+            if(isAdmin)
+            {
+                Entity = new MusicSharingDBEntities();
+            }
+            else
+            {
+                Entity = new MusicSharingDBEntities();
+                
+            }
+        }
+
+    }
+}
